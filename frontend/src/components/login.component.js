3934ce09fc59edc11785b1b702d83dc92d89a5eb
SergioACV <secastrov@unal.edu.co>
2023-11-06 17:20:57 -0500

    Registro
    El sistema puede registrar usuarios. Aun falta que apenas que se creee el usuario, lo redirija a la pagina de inicio de sesión

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -4,9 +4,68 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Axios from 'axios';
+
 
 export default class Login extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+      email: '',
+      password: '',
+      errors: {}
+      
+    };
+
+    
+  }
+
+
   render() {
+
+    const {email, password, errors} = this.state;
+
+    function validation(values){
+      
+      let error = {};
+      error.email="";
+      error.password="";
+
+      // Expresión regular para validar el correo electrónico
+      let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      if (!emailRegex.test(email)) {
+          error.email= "El correo electrónico no es válido.";
+      }
+
+      // La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número
+      let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+      if (!passwordRegex.test(password)) {
+          error.password = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.";
+      }
+
+      return error;
+
+    }
+
+    const handleInput = (event) => {
+      
+      this.setState({
+        [event.target.name]: event.target.value
+       });
+
+    }
+
+    
+    const handleSubmit = (event) => {
+      
+      event.preventDefault();
+      this.setState({ errors: validation(email, password) });
+
+      
+    }
+  
     return (
       <>
         <Container fluid>
@@ -19,28 +78,33 @@ export default class Login extends Component {
               </div>
             </Col>
             <Col className="d-flex justify-content-center align-items-center">
-              <Form className="w-100">
+              <Form className="w-100" onSubmit={handleSubmit}>
                 <h1 class="mb-3">¡Bienvenid@ a Plantastik!</h1>
                 <h5 class="mb-4">
                   Comparte y evalua las experiencias que más te importan.
                 </h5>
                 <Form.Group className="mb-2" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Ingresa tu email" />
+                  <Form.Control 
+                    type="email"
+                    name="email" 
+                    placeholder="Ingresa tu email" 
+                    onChange={handleInput}
+                  />
+                  {errors.email && <span className="text-danger">{errors.email}</span>}
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicPassword">
                   <Form.Control
                     type="password"
+                    name="password"
                     placeholder="Ingresa tu contraseña"
+                    onChange={handleInput}
+                  
                   />
+                  {errors.password && <span className="text-danger">{errors.password}</span>}
                 </Form.Group>
                 <div className="d-grid gap-2">
-                  <Link
-                    className="btn btn-outline-primary"
-                    to={"/user-info"}
-                    size="lg"
-                  >
-                    Iniciar Sesión 
-                  </Link>
+                <input className="btn btn-outline-primary" size="lg" type="submit" value="Iniciar Sesión" />
+                  
                 </div>
                 <Form.Text id="passwordHelpBlock" muted>
                   ¿No tienes una cuenta?{" "}
--- frontend/src/components/register.component.js
+++ frontend/src/components/register.component.js
@@ -1,10 +1,11 @@
 import React, { Component, useState } from "react";
-import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Axios from 'axios';
+
+
 
 export default class Register extends Component {
     constructor(props) {
@@ -16,20 +17,69 @@ export default class Register extends Component {
         gender: '',
         age: '',
         role: 'USER',
+        errors: {}
       };
     }
   render() {
-    const { name, email, password, gender, age, role } = this.state;
+    const { name, email, password, gender, age, role,errors } = this.state;
     
 
-    /*
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [gender,setGender] = useState("");
-    const [age,setAge] = useState("");
-    const [role,setRole] = useState("USER");
-    */
+    function validation(values){
+      
+      let error = {};
+      error.email="";
+      error.password="";
+      error.name="";
+      error.age ="";
+
+      // Validar que el nombre no esté vacío
+      if (!name.trim()) {
+        error.name = "El nombre no puede estar vacío.";
+      }
+
+      if (isNaN(age) || age <= 0) {
+        error.age = "Por favor, introduce una edad válida.";
+      }
+      
+
+      // Expresión regular para validar el correo electrónico
+      let emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      if (!emailRegex.test(email)) {
+          error.email= "El correo electrónico no es válido.";
+      }
+
+      // La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número
+      let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+      if (!passwordRegex.test(password)) {
+          error.password = "La contraseña debe tener al menos 8 caracteres, una letra mayúscula, una letra minúscula y un número.";
+      }
+
+      return error;
+
+    }
+
+    const handleInput = (event) => {
+      
+      this.setState({
+        [event.target.name]: event.target.value
+       });
+
+    }
+
+    const handleSubmit = (event) => {
+      
+      event.preventDefault();
+      this.setState({ errors: validation(name, email,gender,age, password) });
+
+      if(errors.name === "" && errors.email ==="" && errors.password === "" && errors.age === ""){
+        add();
+        
+        
+
+      }
+    }
+    
+
 
     const add = () => {
       Axios.post("http://localhost:3001/create", {
@@ -41,12 +91,16 @@ export default class Register extends Component {
         role: this.state.role,
       }).then(() => {
         alert("USUARIO CREADO");
+        
       });
     };
   
 
 
     return (
+
+
+
       <>
         <Container fluid>
           <Row>
@@ -63,36 +117,54 @@ export default class Register extends Component {
                 Al ser usuario, podrás subir planes y calificar a los que ya
                 fuiste. ¡Comparte tus experiencias con el mundo!
               </h5>
-              <Form className="w-100">
+              <Form className="w-100" onSubmit={handleSubmit}>
                 <Form.Group className="mb-2" controlId="formBasicEmail">
-                  <Form.Control type="text" placeholder="Ingresa tu nombre" 
-                  onChange={(event) => this.setState({ name: event.target.value })} />
+                  <Form.Control 
+                    type="text" 
+                    name="name"
+                    placeholder="Ingresa tu nombre" 
+                    onChange={handleInput} />
+                  {errors.name && <span className="text-danger">{errors.name}</span>}
+                
+
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicEmail">
-                  <Form.Control type="email" placeholder="Ingresa tu email"
-                  onChange={(event) => this.setState({ email: event.target.value })}  />
+                  <Form.Control 
+                    type="email"
+                    name ="email" 
+                    placeholder="Ingresa tu email"
+                    onChange={handleInput} />
+                  {errors.email && <span className="text-danger">{errors.email}</span>}
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicPassword">
                   <Form.Select aria-label="Default select example" 
-                  onChange={(event) => this.setState({ gender: event.target.value })} >
+                    name="gender"
+                    onChange={handleInput} >
                     <option>¿Cuál es tu género?</option>
-                    <option value="1">Masculino</option>
-                    <option value="2">Femenino</option>
-                    <option value="3">
+                    <option value="Masculino">Masculino</option>
+                    <option value="Femenino">Femenino</option>
+                    <option value="Plantastik">
                       Plantastik (prefiero no indicarlo)
                     </option>
                   </Form.Select>
+                  
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicEmail">
-                  <Form.Control type="number" placeholder="Ingresa tu edad" 
-                  onChange={(event) => this.setState({ age: event.target.value })}/>
+                  <Form.Control 
+                    type="number"
+                    name="age"
+                    placeholder="Ingresa tu edad" 
+                    onChange={handleInput}/>
+                  {errors.age && <span className="text-danger">{errors.age}</span>}
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicPassword">
                   <Form.Control
                     type="password"
+                    name="password"
                     placeholder="Crea una contraseña"
-                    onChange={(event) => this.setState({ password: event.target.value })}
+                    onChange={handleInput}
                   />
+                  {errors.password && <span className="text-danger">{errors.password}</span>}
                 </Form.Group>
                 <Form.Group className="mb-2" controlId="formBasicCheckbox">
                   <Form.Check
@@ -101,14 +173,7 @@ export default class Register extends Component {
                   />
                 </Form.Group>
                 <div className="d-grid gap-2">
-                  <Link
-                    className="btn btn-outline-primary"
-                    to={"/user-info"}
-                    size="lg"
-                    onClick={add}
-                  >
-                    Regístrate
-                  </Link>
+                  <input className="btn btn-outline-primary" size="lg" type="submit" value="Registrate" />
                 </div>
               </Form>
             </Col>
